bdb67d8f54933ccbd64e6ee4a32d395105ea7f74
FuzzyKittyz <topher.a@hotmail.com>
2022-10-14 22:57:32 -0600

    adding comments

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,22 +5,23 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+//start pf jquery
 $(document).ready(function() {
-
+//cross site scripting
   const escape = function (str) {
     let div = document.createElement("div");
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
   };
 
-  
+  //function that takes object values and makes html elements to display on the website (the tweets)
   const createTweetElement = function(tweet) {
     let $tweet = $('<article>')
-
+     //html markup to display on the site
     let htmlMarkup = `
     <div class="tweet-header">
       <img src='${tweet.user.avatars}' class="fa-user">
-      ${tweet.user.name}
+     <p> ${tweet.user.name}</p>
       <p class="username">${tweet.user.handle}</p>
     </div>
     <div class='tweet'>
@@ -32,11 +33,11 @@ $(document).ready(function() {
       <i class="fa-solid fa-retweet" class="retweet"></i>
       <i class="fa-solid fa-heart" class="heart"></i>
     </div>`
-
+    //appening it to $tweet (article element)
     $tweet.append(htmlMarkup);
     return $tweet;
   }
-
+ //function that goes throught the tweets object to get the individual tweets and their keys and vlaues
   const renderTweets = function(tweets) {
     for (let tweet of tweets) {
       let $tweet = createTweetElement(tweet)
@@ -45,7 +46,7 @@ $(document).ready(function() {
   };
 
 
-
+ //function that takes loads the tweets using render tweets and using the information gather from /tweets using ajax
   const loadtweets = function(){
     $.ajax('/tweets', {method: 'GET'}).then(function(loadedtweets){
       renderTweets(loadedtweets)
@@ -53,34 +54,45 @@ $(document).ready(function() {
   }
   loadtweets()
 
+//jquery to handle the submit from the text area on the site 
  $('#tweet-form').submit(function(event){
-      event.preventDefault();
-      let hide =document.getElementById('show-hide').style.display = 'none';
-    
-
+      event.preventDefault(); //prevents it from doing the default behaviour (loading another page etc)
+     
+       //if statement checking that the char counter does not pass into the neg
+       //if it does it shows the h3 that says there are too many char 
       if ($('#tweet-form').children('.counter').val() < 0){
-        document.getElementById('show-hide').style.display = 'block'
+        document.getElementById('show-hide1').style.display = 'block'
         setTimeout(() => {
-          document.getElementById('show-hide').style.display = 'none';
+          document.getElementById('show-hide1').style.display = 'none'; //hides the element after 3s using set timeout
+        }, "3000")
+        document.getElementById('tweet-form').reset() //resets the form 
+        return //returns from the function 
+      }
+       //if statments checking if the text feild is empty 
+       //shows message if it is and hides it using timeout
+      if($('#tweet-form').serialize() === 'text='){
+        document.getElementById('show-hide2').style.display = 'block'
+        setTimeout(() => {
+          document.getElementById('show-hide2').style.display = 'none';
+        }, "3000")
+        document.getElementById('tweet-form').reset()
+        return 
+       }
+
+
+      if($('#tweet-form').serialize() === 'text=%0D%0A'){
+        document.getElementById('show-hide2').style.display = 'block'
+        setTimeout(() => {
+          document.getElementById('show-hide2').style.display = 'none';
         }, "3000")
         document.getElementById('tweet-form').reset()
         return 
       }
-
-      if($('#tweet-form').serialize() === 'text='){
-        return alert('No Tweet has been typed!')
-      }
-
-
-      if($('#tweet-form').serialize() === 'text=%0D%0A'){
-        return alert('No Tweet has been typed!')
-      }
-      let ser = ($('#tweet-form').serialize())
-      console.log(ser)
+      let ser = ($('#tweet-form').serialize()) //serializes the tweetform
       $.post('/tweets',ser, function(){
-        loadtweets()
+        loadtweets() //loads the tweet using a post request 
       })
-      document.getElementById('tweet-form').reset();
+      document.getElementById('tweet-form').reset();//resets the form after the process is over
       });
     
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-  let chars = 140;
-  $("textarea").keydown(function() {
-    let amount = $(this).val().length;
+  let chars = 140; 
+  $("textarea").keydown(function() { //use jquery keydown to do pass the function everytime a key is pressed in the text field
+    let amount = $(this).val().length;//the length of textarea value 
     let counter = chars - amount;
-    $(this).parent().children(".counter").text(counter);
+    $(this).parent().children(".counter").text(counter);//looks at the parent elm of textarea(form) and looks for a children with the class .counter
     if (counter > 0) {
-      $(this).parent().children('.counter').addClass('negative');
+      $(this).parent().children('.counter').addClass('negative'); //accounts for if the value is less than zero it goes into the negatives
     }
   })
 });
