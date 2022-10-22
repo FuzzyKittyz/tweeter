294ef22c6f17326ede0cb88de3932acb69cd8733
FuzzyKittyz <topher.a@hotmail.com>
2022-10-22 03:33:02 -0600

    add fix comments in rejected project

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -17,12 +17,12 @@ $(document).ready(function() {
   //function that takes object values and makes html elements to display on the website (the tweets)
   const createTweetElement = function(tweet) {
     //setting up data var which will encompass all of the html markup in an article elm
-    let data = $('<article>');
+    let tweetData = $('<article>');
     //html markup to display on the site
     let htmlMarkup = `
     <div class="tweet-header">
       <img src='${tweet.user.avatars}' class="fa-user">
-     <p> ${tweet.user.name}</p>
+      <p> ${tweet.user.name}</p>
       <p class="username">${tweet.user.handle}</p>
     </div>
     <div class='tweet'>
@@ -35,8 +35,8 @@ $(document).ready(function() {
       <i class="fa-solid fa-heart" class="heart"></i>
     </div>`;
     //appening html markup to data (article element)
-    data.append(htmlMarkup);
-    return data;
+    tweetData.append(htmlMarkup);
+    return tweetData;
   };
   //function that loops the tweets object to get the individual tweets and their keys and vlaues
   const renderTweets = function(tweets) {
@@ -57,16 +57,15 @@ $(document).ready(function() {
   //jquery to handle the submit from the text area on the site
   $('#tweet-form').submit(function(event) {
     event.preventDefault(); //prevents it from doing the default behaviour (loading another page etc)
-    $( "article" ).remove(); //was reloading all the tweets again so everytime remove the article element and reload the tweets obj. Not a great fix but a fix
      
     //if statement checking that the char counter does not pass into the neg
     //if it does it shows the h3 that says there are too many char
-    if ($('#tweet-form').children('.counter').val() < 0) {
+    if ($('#tweet-form').children('.bottem-text').children('.counter').val() < 0) {
       document.getElementById('show-hide1').style.display = 'block';
       setTimeout(() => {
         document.getElementById('show-hide1').style.display = 'none'; //hides the element after 3s using set timeout
       }, "3000");
-      document.getElementById('tweet-form').reset(); //resets the form
+      
       return; //returns from the function
     }
     //if statments checking if the text feild is empty
@@ -90,8 +89,11 @@ $(document).ready(function() {
       document.getElementById('tweet-form').reset();
       return;
     }
-    let ser = ($('#tweet-form').serialize()); //serializes the form text 
-    $.post('/tweets',ser, function() { // takes the serialized tweet and sends it to /tweets
+
+    $( "article" ).remove(); //was reloading all the tweets again so everytime remove the article element and reload the tweets obj. Not a great fix but a fix
+    
+    let serialize = ($('#tweet-form').serialize()); //serializes the form text 
+    $.post('/tweets',serialize, function() { // takes the serialized tweet and sends it to /tweets
       loadtweets(); //loads the tweet from /tweet
     });
     document.getElementById('tweet-form').reset();//resets the form after the process is over
