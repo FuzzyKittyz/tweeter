1afc326c7a8320d9c93ead0893398b488f06ae59
FuzzyKittyz <topher.a@hotmail.com>
2022-10-24 12:55:31 -0600

    add red to negative counter elm

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,8 +4,11 @@ $(document).ready(function() {
     let amount = $(this).val().length;//the length of whats typed in textarea 
     let counter = chars - amount; // the actual value that will show up when you type (the 140 that you start with minus the length of you typing)
     $(this).parent().children('.bottem-text').children(".counter").text(counter);//looks at the parent elm of textarea(form) and looks for a children with a class bottem-text and then a children of that with the class .counter
+    if (counter < 0) { //accounts for if the value is less than zero it changes the counter color to red
+      $(this).parent().children('.bottem-text').children(".counter").addClass('redText');  
+    }
     if (counter > 0) {
-      $(this).parent().children('.counter').addClass('negative'); //accounts for if the value is less than zero it goes into the negatives
+      $(this).parent().children('.bottem-text').children(".counter").removeClass('redText');  
     }
   });
 });
